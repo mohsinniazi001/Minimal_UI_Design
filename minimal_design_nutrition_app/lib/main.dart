9b6ba2252728ef00c1ebb6f619dae39f6862350f
Mohsin Khan Niazi <mohsinniazi818839@gmail.com>
2023-02-08 23:30:13 +0500

    add font family and asset img and other minor design

--- minimal_design_nutrition_app/lib/main.dart
+++ minimal_design_nutrition_app/lib/main.dart
@@ -11,7 +11,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff21bfbd),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15, left: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: (() {}),
+                  icon: const Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 130,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: ((() {})),
+                        icon: const Icon(Icons.filter_list),
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                        onPressed: ((() {})),
+                        icon: const Icon(Icons.menu),
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
