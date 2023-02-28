66c92b6bf727eb95b9954117bc46e451795ceb24
Mohsin Khan Niazi <mohsinniazi818839@gmail.com>
2023-02-28 21:58:34 +0500

    show product in listview

--- minimal_design_nutrition_app/lib/main.dart
+++ minimal_design_nutrition_app/lib/main.dart
@@ -40,27 +40,146 @@ class _MyWidgetState extends State<MyWidget> {
                   icon: const Icon(Icons.arrow_back_ios),
                   color: Colors.white,
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 130,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        onPressed: ((() {})),
-                        icon: const Icon(Icons.filter_list),
-                        color: Colors.white,
-                      ),
-                      IconButton(
-                        onPressed: ((() {})),
-                        icon: const Icon(Icons.menu),
-                        color: Colors.white,
-                      )
-                    ],
-                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: ((() {})),
+                      icon: const Icon(Icons.filter_list),
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      onPressed: ((() {})),
+                      icon: const Icon(Icons.menu),
+                      color: Colors.white,
+                    )
+                  ],
                 )
               ],
             ),
-          )
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: const [
+                Text(
+                  'Healthy',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Food',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Montserrat',
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 185,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(75),
+                )),
+            child: ListView(
+              primary: false,
+              padding: const EdgeInsets.only(left: 20, right: 25),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: .0),
+                  // ignore: sized_box_for_whitespace
+                  child: Container(
+                    height: MediaQuery.of(context).size.height - 300,
+                    child: ListView(children: [
+                      buildFooditems(
+                          "assests/plate1.png", "Salmon Food", '\$2400'),
+                      buildFooditems(
+                          "assests/plate2.png", "Spring Dish", '\$2200'),
+                      buildFooditems(
+                          "assests/plate3.png", "Avocado Dish", '\$2400'),
+                      buildFooditems(
+                          "assests/plate4.png", "Berry Bowl", '\$1800'),
+                    ]),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget buildFooditems(String imgPath, String foodTitle, String price) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 10, top: 10),
+      child: InkWell(
+        onTap: () {},
+        child: Row(
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  Hero(
+                      tag: imgPath,
+                      child: Image(
+                        image: AssetImage(imgPath),
+                        fit: BoxFit.cover,
+                        height: 70,
+                        width: 70,
+                      )),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        foodTitle,
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        price,
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: (() {}),
+              icon: const Icon(Icons.add),
+              color: Colors.black,
+            ),
+          ],
+        ),
       ),
     );
   }
